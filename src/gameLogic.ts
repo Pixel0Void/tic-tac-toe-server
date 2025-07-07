351d869015d68cae6a565ed6cb5910cc74796971
PixelVoid <Pixel0Void@gmail.com>
2025-07-07 15:11:49 +0430

    add function for emitting game state to players in a specific room

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,9 +1,25 @@
 import { DisconnectReason, Socket } from "socket.io";
 import { getRoom } from "./room";
-import { PlayerDisconnectedDto } from "./dataTypes";
+import { GameStateUpdateDto, PlayerDisconnectedDto, PlayerSymbol } from "./dataTypes";
 import { io } from "./index";
 
 const socketRoomMap: { [socketId: string]: string } = {};
+
+function emitFullGameStateUpdate(roomId: string) {
+    const room = getRoom(roomId);
+    if (room) {
+        const gameStateData: GameStateUpdateDto = {
+            board: room.board,
+            scores: { X: room.scores[PlayerSymbol.X]!, O: room.scores[PlayerSymbol.O]! },
+            currentTurn: room.currentTurn,
+            gameState: room.currentGameState,
+            roomId: room.id,
+            playersCount: Object.keys(room.players).length
+        };
+        io.to(roomId).emit("gameStateUpdate", gameStateData);
+        console.log(`[SERVER] [ROOM '${roomId}'] Full game state sent`);
+    }
+}
 
 export function disconnection(socket: Socket, reason: DisconnectReason) {
     console.log(`[SERVER] User disconnected: ${socket.id}, reason: ${reason}`);
