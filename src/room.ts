e8c27c0470a379cd07cbb0260b583e8f346174b6
PixelVoid <Pixel0Void@gmail.com>
2025-07-07 14:32:19 +0430

    add simple room class with required fields

--- src/room.ts
+++ src/room.ts
@@ -1,2 +1,25 @@
+import { GameState, PlayerSymbol } from "./dataTypes";
+
 export class Room {
+    public id: string;
+    public board: PlayerSymbol[];
+    public players: { [id: string]: PlayerSymbol };
+    public playerSockets: { [symbol: string]: string };
+    public currentTurn: PlayerSymbol;
+    public currentGameState: GameState;
+    public scores: { [key in PlayerSymbol]?: number };
+
+    constructor() {
+        this.id = crypto.randomUUID();
+        this.board = Array(9).fill(PlayerSymbol.None);
+        this.players = {};
+        this.playerSockets = {};
+        this.currentTurn = PlayerSymbol.X;
+        this.currentGameState = GameState.WaitingForPlayers;
+        this.scores = {
+            [PlayerSymbol.X]: 0,
+            [PlayerSymbol.O]: 0
+        };
+        console.log(`[Room] Room ${this.id} created.`);
+    }
 }
