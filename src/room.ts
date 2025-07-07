d3f519f77ccecd3ae622e832caae0e4d1c44c258
PixelVoid <Pixel0Void@gmail.com>
2025-07-07 14:42:57 +0430

    add remove and add players to room methods

--- src/room.ts
+++ src/room.ts
@@ -22,6 +22,36 @@ export class Room {
         };
         console.log(`[Room] Room ${this.id} created.`);
     }
+
+    public addPlayer(socketId: string): PlayerSymbol | null {
+        const connectedPlayers = Object.keys(this.players).length;
+        if (connectedPlayers < 2) {
+            const symbol = connectedPlayers === 0 ? PlayerSymbol.X : PlayerSymbol.O;
+            this.players[socketId] = symbol;
+            this.playerSockets[symbol] = socketId;
+            console.log(`[ROOM ${this.id}] Player ${socketId} assigned ${symbol} added.`);
+            if (connectedPlayers + 1 === 2) {
+                this.currentGameState = GameState.Active;
+                console.log(`[ROOM ${this.id}] Game activated.`);
+            }
+            return symbol;
+        }
+        return null;
+    }
+
+    public removePlayer(socketId: string): PlayerSymbol | null {
+        const symbol = this.players[socketId];
+        if (symbol) {
+            delete this.players[socketId];
+            delete this.playerSockets[symbol];
+            console.log(`[ROOM ${this.id}] Player ${symbol} (${socketId}) deleted.`);
+            if (Object.keys(this.players).length === 0) {
+                deleteRoom(this.id);
+            }
+            return symbol;
+        }
+        return null;
+    }
 }
 
 export const rooms: { [id: string]: Room } = {};
